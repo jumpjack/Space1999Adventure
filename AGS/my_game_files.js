9d2c63f1f826ede920aa030746ffd52a3cceb65d
jumpjack <jumpjack@libero.it>
2023-01-12 09:19:16 +0100

    Add files via upload

--- AGS/my_game_files.js
+++ AGS/my_game_files.js
@@ -1,3 +1,1 @@
-var gamefiles = ['acsetup.cfg','my_ags_game.ags'];
-// fill this array with ALL your game files
-// and put them in the same location
+var gamefiles = ['004.ags', 'my_ags_game.ags', 'acsetup.cfg'];
